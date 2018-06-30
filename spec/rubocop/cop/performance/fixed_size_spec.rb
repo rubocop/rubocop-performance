b137a9e01d02ee122146de2a33bf6469a690e098
Daniel Colson <danieljamescolson@gmail.com>
2018-06-29 23:38:01 -0400

    Import RuboCop performance specs
    Includes:
    * RSpec configurition in spec/spec_helper and .rspec,
    * Rakefile with RSpec task
    * .gitignore to ignore tmp files
    * All performance cop specs from RuboCop
    
    These specs are currently passing, since rubocop-performance requires
    rubocop, and rubocop still has all these cops.
    
    When I run the specs I am getting a warning from parser. At
    https://github.com/whitequark/parser/blob/v2.5.1.0/lib/parser/builders/default.rb#L1625
    I am getting warnings like "Unknown escape \m is ignored: /\A\m/". I
    haven't tried to dig into this yet. Does it look familiar?

---
+++ spec/rubocop/cop/performance/fixed_size_spec.rb
@@ -0,0 +1,242 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Performance::FixedSize do
+  subject(:cop) { described_class.new }
+
+  let(:message) do
+    'Do not compute the size of statically sized objects.'
+  end
+
+  shared_examples 'common functionality' do |method|
+    context 'strings' do
+      it "registers an offense when calling #{method} on a single quoted " \
+         'string' do
+        inspect_source("'a'.#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on a double quoted " \
+         'string' do
+        inspect_source("\"a\".#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on a %q string" do
+        inspect_source("%q(a).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on a %Q string" do
+        inspect_source("%Q(a).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on a % string" do
+        inspect_source("%(a).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "accepts calling #{method} on a double quoted string that " \
+         'contains interpolation' do
+        inspect_source("\"\#{foo}\".#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a %Q string that contains " \
+         'interpolation' do
+        inspect_source("\%Q(\#{foo}).#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a % string that contains " \
+         'interpolation' do
+        inspect_source("\%(\#{foo}).#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a single quoted string that " \
+         'is assigned to a constant' do
+        inspect_source("CONST = 'a'.#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a double quoted string that " \
+         'is assigned to a constant' do
+        inspect_source("CONST = \"a\".#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a %q string that is assigned to " \
+         'a constant' do
+        inspect_source("CONST = %q(a).#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a variable " do
+        inspect_source(<<-RUBY.strip_indent)
+          foo = "abc"
+          foo.#{method}
+        RUBY
+
+        expect(cop.messages.empty?).to be(true)
+      end
+    end
+
+    context 'symbols' do
+      it "registers an offense when calling #{method} on a symbol" do
+        inspect_source(":foo.#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on a quoted symbol" do
+        inspect_source(":'foo-bar'.#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "accepts calling #{method} on an interpolated quoted symbol" do
+        inspect_source(":\"foo-\#{bar}\".#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "registers an offense when calling #{method} on %s" do
+        inspect_source("%s(foo-bar).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "accepts calling #{method} on a symbol that is assigned " \
+         'to a constant' do
+        inspect_source("CONST = :foo.#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+    end
+
+    context 'arrays' do
+      it "registers an offense when calling #{method} on an array using []" do
+        inspect_source("[1, 2, foo].#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on an array using %w" do
+        inspect_source("%w(1, 2, foo).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "registers an offense when calling #{method} on an array using %W" do
+        inspect_source("%W(1, 2, foo).#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "accepts calling #{method} on an array using [] that contains " \
+         'a splat' do
+        inspect_source("[1, 2, *foo].#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on array that is set to a variable" do
+        inspect_source(<<-RUBY.strip_indent)
+          foo = [1, 2, 3]
+          foo.#{method}
+        RUBY
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on an array that is assigned " \
+         'to a constant' do
+        inspect_source("CONST = [1, 2, 3].#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+    end
+
+    context 'hashes' do
+      it "registers an offense when calling #{method} on a hash using {}" do
+        inspect_source("{a: 1, b: 2}.#{method}")
+
+        expect(cop.messages).to eq([message])
+      end
+
+      it "accepts calling #{method} on a hash set to a variable" do
+        inspect_source(<<-RUBY.strip_indent)
+          foo = {a: 1, b: 2}
+          foo.#{method}
+        RUBY
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on a hash that contains a double splat" do
+        inspect_source("{a: 1, **foo}.#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+
+      it "accepts calling #{method} on an hash that is assigned " \
+         'to a constant' do
+        inspect_source("CONST = {a: 1, b: 2}.#{method}")
+
+        expect(cop.messages.empty?).to be(true)
+      end
+    end
+  end
+
+  it_behaves_like 'common functionality', 'size'
+  it_behaves_like 'common functionality', 'length'
+  it_behaves_like 'common functionality', 'count'
+
+  shared_examples 'count with arguments' do |variable|
+    it 'accepts calling count with a variable' do
+      inspect_source("#{variable}.count(bar)")
+
+      expect(cop.messages.empty?).to be(true)
+    end
+
+    it 'accepts calling count with an instance variable' do
+      inspect_source("#{variable}.count(@bar)")
+
+      expect(cop.messages.empty?).to be(true)
+    end
+
+    it 'registers an offense when calling count with a string' do
+      inspect_source("#{variable}.count('o')")
+
+      expect(cop.messages).to eq([message])
+    end
+
+    it 'accepts calling count with a block' do
+      inspect_source("#{variable}.count { |v| v == 'a' }")
+
+      expect(cop.messages.empty?).to be(true)
+    end
+
+    it 'accepts calling count with a symbol proc' do
+      inspect_source("#{variable}.count(&:any?) ")
+
+      expect(cop.messages.empty?).to be(true)
+    end
+  end
+
+  it_behaves_like 'count with arguments', '"foo"'
+  it_behaves_like 'count with arguments', '[1, 2, 3]'
+  it_behaves_like 'count with arguments', '{a: 1, b: 2}'
+end
---
+++ spec/rubocop/cop/performance/sample_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Performance::Sample do
+  subject(:cop) { described_class.new }
+
+  shared_examples 'offense' do |wrong, right|
+    it "when using #{wrong}" do
+      inspect_source("[1, 2, 3].#{wrong}")
+      expect(cop.messages).to eq(["Use `#{right}` instead of `#{wrong}`."])
+    end
+
+    context 'corrects' do
+      it "#{wrong} to #{right}" do
+        new_source = autocorrect_source("[1, 2, 3].#{wrong}")
+        expect(new_source).to eq("[1, 2, 3].#{right}")
+      end
+    end
+  end
+
+  shared_examples 'accepts' do |acceptable|
+    it acceptable do
+      inspect_source("[1, 2, 3].#{acceptable}")
+      expect(cop.messages.empty?).to be(true)
+    end
+  end
+
+  it_behaves_like('offense', 'shuffle.first', 'sample')
+  it_behaves_like('offense', 'shuffle.last', 'sample')
+  it_behaves_like('offense', 'shuffle[0]', 'sample')
+  it_behaves_like('offense', 'shuffle[0, 3]', 'sample(3)')
+  it_behaves_like('offense', 'shuffle[0..3]', 'sample(4)')
+  it_behaves_like('offense', 'shuffle[0...3]', 'sample(3)')
+  it_behaves_like('offense', 'shuffle.first(2)', 'sample(2)')
+  it_behaves_like('offense', 'shuffle.last(3)', 'sample(3)')
+  it_behaves_like('offense', 'shuffle.first(foo)', 'sample(foo)')
+  it_behaves_like('offense', 'shuffle.last(bar)', 'sample(bar)')
+  it_behaves_like('offense', 'shuffle(random: Random.new).first',
+                  'sample(random: Random.new)')
+  it_behaves_like('offense', 'shuffle(random: Random.new).first(2)',
+                  'sample(2, random: Random.new)')
+  it_behaves_like('offense', 'shuffle(random: foo).last(bar)',
+                  'sample(bar, random: foo)')
+  it_behaves_like('offense', 'shuffle(random: Random.new)[0..3]',
+                  'sample(4, random: Random.new)')
+
+  it_behaves_like('accepts', 'sample')
+  it_behaves_like('accepts', 'shuffle')
+  it_behaves_like('accepts', 'shuffle[2]')            # nil if coll.size < 3
+  it_behaves_like('accepts', 'shuffle[3, 3]')         # nil if coll.size < 3
+  it_behaves_like('accepts', 'shuffle[2..3]')         # empty if coll.size < 3
+  it_behaves_like('accepts', 'shuffle[2..-3]')        # can't compute range size
+  it_behaves_like('accepts', 'shuffle[foo..3]')       # can't compute range size
+  it_behaves_like('accepts', 'shuffle[-4..-3]')       # nil if coll.size < 3
+  it_behaves_like('accepts', 'shuffle[foo]')          # foo could be a Range
+  it_behaves_like('accepts', 'shuffle[foo, 3]')       # nil if coll.size < foo
+  it_behaves_like('accepts', 'shuffle[foo..bar]')
+  it_behaves_like('accepts', 'shuffle[foo, bar]')
+  it_behaves_like('accepts', 'shuffle(random: Random.new)')
+  it_behaves_like('accepts', 'shuffle.join([5, 6, 7])')
+  it_behaves_like('accepts', 'shuffle.map { |e| e }')
+  it_behaves_like('accepts', 'shuffle(random: Random.new)[2]')
+  it_behaves_like('accepts', 'shuffle(random: Random.new)[2, 3]')
+  it_behaves_like('accepts', 'shuffle(random: Random.new).find(&:odd?)')
+end
