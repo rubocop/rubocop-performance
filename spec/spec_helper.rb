8f6041bb2acd87c4a1c333a7af51b845ea01e30a
Koichi ITO <koic.ito@gmail.com>
2018-07-14 04:45:58 +0900

    Change `example_status_persistence_file_path`
    Follow up of https://github.com/rubocop-hq/rubocop-performance/commit/7458272#diff-a084b794bc0759e7a6b77810e01874f2R21
    
    This path is the same as the path in RSpec official document.
    http://rspec.info/blog/2015/06/rspec-3-3-has-been-released/#core-new---only-failures-option
    
    It was introduced in RuboCop Core below.
    rubocop-hq/rubocop@3f97ff1.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = 'tmp/examples.txt'
+  config.example_status_persistence_file_path = 'spec/examples.txt'
   config.disable_monkey_patching!
   config.warnings = true
 
