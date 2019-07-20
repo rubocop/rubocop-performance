c761793bb32c1cba2bb1ae27a17589aeb964512e
Koichi ITO <koic.ito@gmail.com>
2019-07-20 09:00:00 +0900

    Reduce .rubocop_todo.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,7 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
