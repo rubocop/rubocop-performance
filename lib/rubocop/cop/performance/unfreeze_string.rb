4ac19f12d3294e7c49c7fe80978f216c53ddcbaa
Koichi ITO <koic.ito@gmail.com>
2020-05-31 16:57:52 +0900

    `rake generate_cops_documentation` generates AsciiDoc
    Follow up rubocop-hq/rubocop#8047 and rubocop-hq/rubocop#8067.

--- lib/rubocop/cop/performance/unfreeze_string.rb
+++ lib/rubocop/cop/performance/unfreeze_string.rb
@@ -7,7 +7,7 @@ module RuboCop
       # literal instead of `String#dup` and `String.new`.
       # Unary plus operator is faster than `String#dup`.
       #
-      # Note: `String.new` (without operator) is not exactly the same as `+''`.
+      # NOTE: `String.new` (without operator) is not exactly the same as `+''`.
       # These differ in encoding. `String.new.encoding` is always `ASCII-8BIT`.
       # However, `(+'').encoding` is the same as script encoding(e.g. `UTF-8`).
       # So, if you expect `ASCII-8BIT` encoding, disable this cop.
