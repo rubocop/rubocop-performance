4dc1e5c5d6e0761d7d783ec543f6982e8a369ed0
Parker Finch <pfinch@panoramaed.com>
2021-05-05 08:00:23 -0400

    [Fix #240] Disable `Performance/Casecmp` cop
    This cop suggests an incorrect fix when code involves non-ASCII
    characters. Disabling it by default makes this risky behavior opt-in
    rather than opt-out.

--- lib/rubocop/cop/performance/casecmp.rb
+++ lib/rubocop/cop/performance/casecmp.rb
@@ -6,6 +6,12 @@ module RuboCop
       # Identifies places where a case-insensitive string comparison
       # can better be implemented using `casecmp`.
       #
+      # This cop is disabled by default because `String#casecmp` only works with
+      # ASCII characters. See https://github.com/rubocop/rubocop/issues/9753.
+      #
+      # If you are working only with ASCII characters, then this cop can be
+      # safely enabled.
+      #
       # @safety
       #   This cop is unsafe because `String#casecmp` and `String#casecmp?` behave
       #   differently when using Non-ASCII characters.
