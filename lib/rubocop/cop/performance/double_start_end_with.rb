a48cf8372059d5de41a2febd284ca80b3de6fc37
Koichi ITO <koic.ito@gmail.com>
2021-11-22 02:31:41 +0900

    Add examples for `IncludeActiveSupportAliases` option of `Performance/DoubleStartEndWith` cop

--- lib/rubocop/cop/performance/double_start_end_with.rb
+++ lib/rubocop/cop/performance/double_start_end_with.rb
@@ -7,6 +7,9 @@ module RuboCop
       # separated by `||`. In some cases such calls can be replaced
       # with an single `#start_with?`/`#end_with?` call.
       #
+      # `IncludeActiveSupportAliases` configuration option is used to check for
+      # `starts_with?` and `ends_with?`. These methods are defined by Active Support.
+      #
       # @example
       #   # bad
       #   str.start_with?("a") || str.start_with?(Some::CONST)
@@ -17,6 +20,24 @@ module RuboCop
       #   str.start_with?("a", Some::CONST)
       #   str.start_with?("a", "b", "c")
       #   str.end_with?(var1, var2)
+      #
+      # @example IncludeActiveSupportAliases: false (default)
+      #   # good
+      #   str.starts_with?("a", "b") || str.starts_with?("c")
+      #   str.ends_with?(var1) || str.ends_with?(var2)
+      #
+      #   str.starts_with?("a", "b", "c")
+      #   str.ends_with?(var1, var2)
+      #
+      # @example IncludeActiveSupportAliases: true
+      #   # bad
+      #   str.starts_with?("a", "b") || str.starts_with?("c")
+      #   str.ends_with?(var1) || str.ends_with?(var2)
+      #
+      #   # good
+      #   str.starts_with?("a", "b", "c")
+      #   str.ends_with?(var1, var2)
+      #
       class DoubleStartEndWith < Base
         extend AutoCorrector
 
