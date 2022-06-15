6777b43fe98121b5f7427d6853c935ab8418c5ec
Koichi ITO <koic.ito@gmail.com>
2022-06-16 08:46:53 +0900

    Add settings to `Naming/InclusiveLanguage`
    Follow up https://github.com/rubocop/rubocop/pull/10714.

--- lib/rubocop/cop/performance/sum.rb
+++ lib/rubocop/cop/performance/sum.rb
@@ -7,7 +7,7 @@ module RuboCop
       # in some Enumerable object can be replaced by `Enumerable#sum` method.
       #
       # @safety
-      #   Auto-corrections are unproblematic wherever an initial value is provided explicitly:
+      #   Autocorrections are unproblematic wherever an initial value is provided explicitly:
       #
       #   [source,ruby]
       #   ----
@@ -43,7 +43,7 @@ module RuboCop
       #
       # @example OnlySumOrWithInitialValue: false (default)
       #   # bad
-      #   [1, 2, 3].inject(:+)                        # Auto-corrections for cases without initial value are unsafe
+      #   [1, 2, 3].inject(:+)                        # Autocorrections for cases without initial value are unsafe
       #   [1, 2, 3].inject(&:+)                       # and will only be performed when using the `-A` option.
       #   [1, 2, 3].reduce { |acc, elem| acc + elem } # They can be prohibited completely using `SafeAutoCorrect: true`.
       #   [1, 2, 3].reduce(10, :+)
