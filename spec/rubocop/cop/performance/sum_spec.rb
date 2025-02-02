074bd2aef174dd5067ba427e82dc5aa3cb2f8f31
Koichi ITO <koic.ito@gmail.com>
2025-02-03 03:44:29 +0900

    Explicit default `Performance/Sum` cop config for the spec
    If not explicitly specified, it depends on `config/default.yml`,
    which is the result of configuration file loading as a side effect of the Inject mechanism.

--- spec/rubocop/cop/performance/sum_spec.rb
+++ spec/rubocop/cop/performance/sum_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Performance::Sum, :config do
+  let(:cop_config) { { 'SafeAutoCorrect' => false, 'OnlySumOrWithInitialValue' => false } }
+
   %i[inject reduce].each do |method|
     it "registers an offense and corrects when using `array.#{method}(10, :+)`" do
       expect_offense(<<~RUBY, method: method)
